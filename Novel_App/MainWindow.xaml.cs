1baa45444d70c58108524df914ed73f866dd469d
xuanthinhdeptraivaiohhhhh <xuanthinh311bl@gmail.com>
2025-03-24 07:32:33 +0700

    Revert "đăng ký"
    This reverts commit 58dcab57ed679be3256fe512bd6145591e74266b.

--- Novel_App/MainWindow.xaml.cs
+++ Novel_App/MainWindow.xaml.cs
@@ -33,10 +33,6 @@ namespace Novel_App
         {
             MainFrame.Navigate(new GenreView());
         }
-
-        private void Register_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Navigate(new Register());
-        }
+        
     }
 }
